b571bbb72d6c6545eb68aa232ef4f74cf9f97505
Thwmas Fysekis <thwmas312@gmail.com>
2023-05-17 23:26:35 +0300

    schedule(A,B,C) made with file and not lists,started scedule_errors

--- exam_schedule.pl
+++ exam_schedule.pl
@@ -1,49 +1,34 @@
 % AEM: 3770, Φυσέκης Θωμάς
 % AEM: 3620, Στέργιος Μουμτζής
 /*
-len_combination(Len, Lst, Comb) :-
-    length(Comb, Len),
-    len_combination_(Comb, Lst).
+% Define the valid_schedule predicate
+valid_schedule(A, B, C) :-
+    % Check that no lesson appears more than once in the schedule
+    setof(L, (member(L, A); member(L, B); member(L, C)), Ls),
+    length(Ls, N),
+    length(A, Na),
+    length(B, Nb),
+    length(C, Nc),
+    % Check that all three weeks have the correct number of unique lessons
+    N =:= Na + Nb + Nc.
 
-combinations([], []).
-combinations([H|T], [H|T2]) :-
-    combinations(T, T2).
-combinations([_|T], T2) :-
-    combinations(T, T2).
-
-all_combinations(L) :- L = [A,B,C,D,E,F,G,H],
-                       member(A,[im204, im209, im210, im212, im214, im216, im217, im218]),
-                       member(B,[im204, im209, im210, im212, im214, im216, im217, im218]),
-                       B \= A,
-                       member(C,[im204, im209, im210, im212, im214, im216, im217, im218]),
-                       C \= A, C \= B,
-                       member(D,[im204, im209, im210, im212, im214, im216, im217, im218]),
-                       D \= A, D \= B, D \= C,
-                       member(E,[im204, im209, im210, im212, im214, im216, im217, im218]),
-                       E \= A, E \= B, E \= C, E \= D,
-                       member(F,[im204, im209, im210, im212, im214, im216, im217, im218]),
-                       F \= A, F \= B, F \= C, F \= D, F \= E,
-                       member(G,[im204, im209, im210, im212, im214, im216, im217, im218]),
-                       G \= A, G \= B, G \= C, G \= D, G \= E, G \= F,
-                       member(H,[im204, im209, im210, im212, im214, im216, im217, im218]),
-                       H \= A, H \= B, H \= C, H \= D, H \= E, H \= F, H \= G.
-
+% Check that each group has the right number of lessons
+length(A,3),
+length(B,3),
+length(C,2).
+% Check that the schedule satisfies the constraints
+valid_schedule(A, B, C).
 */
 
-%% Define the lessons
-lessons([im204, im209, im210, im212, im214, im216, im217, im218]).
+%oad the attends.pl file
+:- consult('attends.pl').
 
 schedule(A, B, C) :-
-    % Get the list of all lessons
-    lessons(L),
-    % Try all possible schedules using the remaining lessons
-    schedule_(L, A, B, C).
-    % Check that each group has the right number of lessons
-    length(A,3),
-    length(B,3),
-    length(C,2).
-    % Check that the schedule satisfies the constraints
-    valid_schedule(A, B, C).
+    % Get the list of all lessons from attends.pl without duplicates
+    findall(Lesson, attends(_, Lesson), Lessons),
+    list_to_set(Lessons, UniqueLessons),
+    % Try all possible schedules using the unique lessons
+    schedule_(UniqueLessons, A, B, C).
 
 % Define the schedule_ predicate for backtracking
 schedule_([], [], [], []).
@@ -70,14 +55,14 @@ schedule_([L|Ls], A, B, C) :-
     C = [L|C1].
 
 
-% Define the valid_schedule predicate
-valid_schedule(A, B, C) :-
-    % Check that no lesson appears more than once in the schedule
-    setof(L, (member(L, A); member(L, B); member(L, C)), Ls),
-    length(Ls, N),
-    length(A, Na),
-    length(B, Nb),
-    length(C, Nc),
-    % Check that all three weeks have the correct number of unique lessons
-    N =:= Na + Nb + Nc.
 
+% Define the schedule_errors predicate
+schedule_errors(A, B, C, E) :-
+    findall(Students, attends(Students,A), StudentsList),
+    write(StudentsList).%not gonna work,the A has 3 elements not 1
+    %count_dissatisfied_students(A, B, C, E).
+
+% Define the count_dissatisfied_students predicate
+count_dissatisfied_students(A, B, C, E) :-
+    findall(S, (attends(S, L), (member(L, A); member(L, B); member(L, C))), Students),
+    findall(Student, (member(Student, Students), count_occurrences(Student, Students, Occurrences))).
